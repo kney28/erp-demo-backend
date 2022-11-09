8101b0d784c9c8a19fca4a1cfcca5c621cc26dd1
Daniel Gómez <daniel.gomez@itagui.gov.co>
2022-11-08 20:19:26 -0500

    se resuelve la tarea #EH-14

---
+++ src/departments/entities/department.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Department {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ length: 10, unique: true })
+  codigo: string;
+
+  @Column({ length: 60 })
+  descripcion: string;
+
+  @Column('int')
+  estado: number;
+
+  @CreateDateColumn()
+  fecha: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+
+  @ManyToOne(() => User, (user: User) => user)
+  public usuario: User;
+}
