e08c637081eb00b37bf99386031fc0661a14ed96
Daniel Gómez <daniel.gomez@itagui.gov.co>
2022-12-01 12:29:18 -0500

    se resuelve la tarea #EH-54

--- src/types-seats/types-seats.module.ts
+++ src/types-seats/types-seats.module.ts
@@ -3,11 +3,17 @@ import { TypesSeatsService } from './types-seats.service';
 import { TypesSeatsController } from './types-seats.controller';
 import { TypesSeat } from './entities/types-seat.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypesSeatlogs } from './entities/types-seatslogs.entity';
+import { TypesSeatSubscriber } from './entities/types-seats.subscriber';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TypesSeat]), TypesSeatsModule],
+  imports: [
+    TypeOrmModule.forFeature([TypesSeat]),
+    TypesSeatsModule,
+    TypeOrmModule.forFeature([TypesSeatlogs]),
+  ],
   controllers: [TypesSeatsController],
-  providers: [TypesSeatsService],
+  providers: [TypesSeatsService, TypesSeatSubscriber],
   exports: [TypesSeatsService],
 })
 export class TypesSeatsModule {}
