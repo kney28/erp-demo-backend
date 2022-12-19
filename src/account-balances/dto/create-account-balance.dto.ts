d2d58b23bb2d7c1f58d7c1b09bc3de2eb5599953
Daniel Gómez <daniel.gomez@itagui.gov.co>
2022-12-19 12:14:45 -0500

    fix

--- src/account-balances/dto/create-account-balance.dto.ts
+++ src/account-balances/dto/create-account-balance.dto.ts
@@ -1,8 +1,17 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty } from 'class-validator';
+import { ThirdPersonExist } from 'src/thirdPersons/validations/thirdPersons.validate.unique';
 
 export class CreateAccountBalanceDto {
   @IsNotEmpty({
     message: 'El campo mes no puede estar vacio.',
   })
   mes: number;
+
+  @ThirdPersonExist({
+    message: 'El id del tercero, no existe.',
+  })
+  thirdId: number;
+
+  debit: number;
+  credit: number;
 }
