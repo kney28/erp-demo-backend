9d8b4c9d3952ab14f8abd5986e8cb2176526a317
Oscar Eduardo Román Cerón <oscar.roma@utp.edu.co>
2022-12-29 01:25:42 -0500

    Agrego modelo-controlador de detalle de copagos y cuotas moderadoras

---
+++ src/detail-copays-and-fees/dto/update-detailCopaysAndFee.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateDetailCopaysAndFeeDto } from './create-detailCopaysAndFee.dto';
+
+export class UpdateDetailCopaysAndFeeDto extends PartialType(CreateDetailCopaysAndFeeDto) {}
---
+++ src/detail-copays-and-fees/entities/detailCopaysAndFee.entity.ts
@@ -0,0 +1,42 @@
+import { BaseEntity } from 'src/base/baseEntity';
+import { Column, Entity } from 'typeorm';
+
+export enum DetailCopaysAndFeeType {
+  MODERATORFEE = 1,
+  COPAY = 2,
+  SUBSIDIZEDREGIME = 3,
+  LINKED = 4,
+}
+
+export enum DetailCopaysAndFeeStatus {
+  ACTIVE = 1,
+  INACTIVE = 2,
+}
+
+@Entity()
+export class DetailCopaysAndFee extends BaseEntity {
+  @Column()
+  copaysandfees: number;
+
+  @Column({
+    type: 'enum',
+    enum: DetailCopaysAndFeeType,
+  })
+  kind: DetailCopaysAndFeeType;
+
+  @Column()
+  percentage: number;
+
+  @Column()
+  capevent: number;
+
+  @Column()
+  annualcap: number;
+
+  @Column({
+    type: 'enum',
+    enum: DetailCopaysAndFeeStatus,
+    default: DetailCopaysAndFeeStatus.ACTIVE,
+  })
+  status: DetailCopaysAndFeeStatus;
+}
