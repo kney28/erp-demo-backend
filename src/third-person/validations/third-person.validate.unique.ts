a1310098a81d341bb26833d25ebe47a576e91c60
Oscar Eduardo Román Cerón <oscar.roma@utp.edu.co>
2022-12-30 00:29:54 -0500

    Solución de problemas de código

---
+++ src/third-person/validations/third-person.validate.unique.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { selectionCatalog } from 'src/account-catalog/entities/account-catalog.entity';
+import { ThirdPersonService } from 'src/third-person/third-person.service';
+
+@ValidatorConstraint({ name: 'ThirdPersonExist', async: true })
+@Injectable()
+export class ThirdPersonExistConstraint
+  implements ValidatorConstraintInterface
+{
+  constructor(protected readonly thirdPersonsService: ThirdPersonService) {}
+  async validate(id: string, args: ValidationArguments) {
+    try {
+      const data = JSON.stringify(args.object);
+      const dataJson = JSON.parse(data);
+      if (dataJson.transferThirdParties == selectionCatalog.YES && !id) {
+        return false;
+      }
+      if (!id) {
+        return true;
+      }
+      return await this.thirdPersonsService.findOneById(id).then((third) => {
+        if (third) return true;
+        return false;
+      });
+    } catch (error) {
+      return false;
+    }
+  }
+}
+export function ThirdPersonExist(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: ThirdPersonExistConstraint,
+    });
+  };
+}
