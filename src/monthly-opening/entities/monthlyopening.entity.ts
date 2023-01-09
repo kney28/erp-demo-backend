b2ddbecb2514b9caad8350e60f006fd90f6ea641
Oscar Eduardo Román Cerón <oscar.roma@utp.edu.co>
2023-01-09 16:41:35 -0500

    Agrego tabla y modelo de la apertura mensual

---
+++ src/monthly-opening/entities/monthlyopening.entity.ts
@@ -0,0 +1,8 @@
+import { BaseEntity } from 'src/base/baseEntity';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class MonthlyOpening extends BaseEntity {
+  @Column()
+  month: string;
+}
